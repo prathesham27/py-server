2bd855274906d1e2d9932c58ee8dadf7d62a961e
Prathesha <prathesha.m27@gmail.com>
2020-11-27 10:29:04 +0530

    main

--- simple.py
+++ simple.py
@@ -2,7 +2,16 @@ mylist = input("Enter The Input:")
 list = mylist.split()
 print("Input:", list)
 for x in mylist:
-    if len(set(list)) == len(list):
-        print("True")
-    else:
-        print("False")
+    def unique(list):
+        if len(set(list)) == len(list):
+            return True
+        else:
+            return False
+if(unique (list) == True):
+    print("TRUE")
+elif(unique (list) == False):
+    print("FALSE")
+
+
+
+
