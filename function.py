edb2ce1689d7b76acb478ec56b9422593f4bcec1
Prathesha <prathesha.m27@gmail.com>
2021-03-07 22:23:18 +0530

    array changes

---
+++ function.py
@@ -0,0 +1,14 @@
+array=[1,2,3,4,5]
+def insert(element):
+    array.append(element);
+def remove(element):
+    array.remove(element);
+def pop():
+    array.pop();
+insert(23)
+print(array)
+remove(2)
+print(array)
+pop()
+print(array)
+
